da931de383df2a08930109fa936a9c0759064d11
Kehinde Olaleye <simeonkenny66@gmail.com>
2023-07-24 14:44:55 +0100

    effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Api from './assets/components/Api'
+import Effect from './assets/components/Effect'
 import Herosection from './assets/components/Herosection'
 import Navbar from './assets/components/Navbar'
 import Signup from './assets/components/Signup'
@@ -11,47 +12,48 @@ function App() {
     fontFamily: 'cursive'
   }
 
-  let studentArray = ["John", "OluwaFemi", "Kayode", "Simeon"]
+  // let studentArray = ["John", "OluwaFemi", "Kayode", "Simeon"]
   // console.log(studentArray);
 
-  let anotherArray = [
-    {
-      Name:"John",
-      School:"SQI",
-      Age:"211"
-    },
-    {
-      Name:"Olu",
-      School:"SQI",
-      Age:"211"
-    },
-    {
-      Name:"Kayode",
-      School:"SQI",
-      Age:"211"
-    },
-    {
-      Name:"Simeon",
-      School:"SQI",
-      Age:"211"
-    }
-  ]
+  // let anotherArray = [
+  //   {
+  //     Name:"John",
+  //     School:"SQI",
+  //     Age:"211"
+  //   },
+  //   {
+  //     Name:"Olu",
+  //     School:"SQI",
+  //     Age:"211"
+  //   },
+  //   {
+  //     Name:"Kayode",
+  //     School:"SQI",
+  //     Age:"211"
+  //   },
+  //   {
+  //     Name:"Simeon",
+  //     School:"SQI",
+  //     Age:"211"
+  //   }
+  // ]
   return (
     <>
       {/* <Navbar/> */}
       {/* <Herosection/> */}
       {/* <Signup/> */}
-      <Api/>
+      {/* <Api/> */}
+      <Effect/>
 
       {
-        anotherArray.map((item,index)=>(
-          <>
-          <small>{index+1}</small>
-          <h1>{item.Name}</h1>
-          <h1>{item.School}</h1>
-          <h1>{item.age}</h1>
-          </>
-        ))
+        // anotherArray.map((item,index)=>(
+        //   <>
+        //   <small>{index+1}</small>
+        //   <h1>{item.Name}</h1>
+        //   <h1>{item.School}</h1>
+        //   <h1>{item.age}</h1>
+        //   </>
+        // ))
       }
     </>
   )
---
+++ src/assets/components/Effect.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+
+
+// import React from 'react'
+
+const Effect = () => {
+    useEffect(() => {
+        console.log("Changing");
+      }, [])
+
+      const [first, setFirst] = useState(0)
+
+      const change = () => {
+        setFirst(first+1)
+      }
+  return (
+    <>
+    <div>{first}</div>
+    <button onClick={change}>Change</button>
+    </>
+    
+  )
+}
+
+export default Effect
